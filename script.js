2f03b164940f46ba51054c11aa0ade30135eb65b
Filipe Sales <devfilipesales@gmail.com>
2020-01-08 23:50:38 -0300

    Player e drawMatrix

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const canvas = document.getElementById("tetris");
-const ctx = canvas.getContext('2d')
+const ctx = canvas.getContext('2d');
 
 ctx.scale(20, 20);
 
-ctx.fillRect(0, 0, 1, 1);
+// ctx.fillRect(0, 0, 1, 1);
 
 const pieces = [
     [
@@ -42,3 +42,18 @@ const pieces = [
         [0, 1, 0]
     ],
 ];
+const arena = [];
+
+const player = {
+    pos: { x: 0, y: 0 },
+    matrix: pieces[3]
+}
+
+function drawMatrix(matrix, x, y) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j])
+                ctx.fillRect(x + j, y + i, 1, 1);
+        }
+    }
+}
