12b0b9faf9532c2b71ecf43f6289452cf66bfde5
Filipe Sales <devfilipesales@gmail.com>
2020-01-09 00:04:42 -0300

    Animação da Peça Caindo

--- script.js
+++ script.js
@@ -11,36 +11,36 @@ const pieces = [
         [1, 1]
     ],
     [
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 0, 0],
-        [0, 1, 0, 0]
+        [0, 2, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 0, 0],
+        [0, 2, 0, 0]
     ],
     [
         [0, 0, 0],
-        [1, 1, 0],
-        [0, 1, 1]
+        [3, 3, 0],
+        [0, 3, 3]
     ],
     [
         [0, 0, 0],
-        [0, 1, 1],
-        [1, 1, 0]
+        [0, 4, 4],
+        [4, 4, 0]
     ],
     [
-        [1, 0, 0],
-        [1, 0, 0],
-        [1, 1, 0]
+        [5, 0, 0],
+        [5, 0, 0],
+        [5, 5, 0]
     ],
     [
-        [0, 0, 1],
-        [0, 0, 1],
-        [0, 1, 1]
+        [0, 0, 6],
+        [0, 0, 6],
+        [0, 6, 6]
     ],
     [
         [0, 0, 0],
-        [1, 1, 1],
-        [0, 1, 0]
-    ],
+        [7, 7, 7],
+        [0, 7, 0]
+    ]
 ];
 const arena = [];
 
@@ -57,3 +57,30 @@ function drawMatrix(matrix, x, y) {
         }
     }
 }
+
+let interval = 1000;
+let lastTime = 0;
+let count = 0;
+
+function update(time = 0) {
+
+    const dt = time - lastTime;
+    lastTime = time;
+    count += dt;
+
+    if (count >= interval) {
+        player.pos.y++;
+        count = 0;
+    }
+
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "red";
+    drawMatrix(player.matrix, player.pos.x, player.pos.y);
+
+    requestAnimationFrame(update);
+
+}
+
+update();
